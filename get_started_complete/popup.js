36fd1568a0325b78c048a83f88dd2194c620bc63
Héctor <hector.martos@nfq.es>
2019-06-09 05:41:12 +0200

    Added button to go to stats page into popup

--- get_started_complete/popup.js
+++ get_started_complete/popup.js
@@ -4,66 +4,57 @@
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
+let goToStatsBtn = document.getElementById('goToStatsBtn');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-chrome.runtime.getBackgroundPage(function (bg) {
-    let activity = document.getElementById('activity');
-    activity.innerHTML = JSON.stringify(bg.activity);
-
-    let lodash = document.getElementById('lodash');
-    lodash.innerHTML = _.partition([1, 2, 3, 4], n => n % 2);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  //console.log(`Color chnaged to ${color}`)
+goToStatsBtn.onclick = function(element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: `location.replace('https://www.netflix.com/viewingactivity?stats=true')`});
-        //{code: 'document.body.style.backgroundColor = "' + color + '";'});
+        {code: `location.replace('https://www.netflix.com/viewingactivity?stats=true')`}
+    );
   });
 };
 
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+chrome.tabs.executeScript({
+    code: '(' + function(params) {
+        return {language: document.querySelector('html').getAttribute('lang') || 'en'};
+    } + ')();'
+}, function(results) {
+    console.log("Language: " + results[0].language);
+    initializeLanguage(results[0].language);
 });
+
+/**
+ * Initialize i18n with given language
+ * @param {*} language 
+ */
+function initializeLanguage(language) {
+    i18next.init({
+      lng: language,
+      debug: true,
+      resources: {
+        en: {
+          translation: {
+            "goToStats": "SHOW STATS",
+          }
+        },
+        es: {
+          translation: {
+            "goToStats": "VER ESTADÍSTICAS",
+          }
+        }
+      }
+    }, function(error, t) {
+      if (error) {
+        console.error(error);
+      }
+      translatePage();
+    });
+  }
+
+  /**
+ * Translate texts in page
+ */
+function translatePage() {
+    document.querySelector('#goToStatsBtn span').innerHTML = i18next.t('goToStats');
+}  
